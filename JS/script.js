fbe32126d512da8b6f50dfe67a0b16fa76fed997
lazbek <jannnjan592@gmail.com>
2022-01-27 11:51:57 +0500

    lazbek

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+
+// let ism = prompt('ismingizni kiriting');
+// let fam = prompt('familyangizni kiriting');
+
+// alert('misollar yechishgan tayyrmisiz');
+// console.log(ism);
+// console.log(fam);
+// alert(`unda hurmatli ${ism} birgalikda boshladik`);
+
+let misol = prompt('20+30 =');
+// let misolSecond = prompt('49+30 =');
+// let misolThird = prompt('60+40 =');
+// let misolFourth = prompt('120+40 =');
+// let misolFifth = prompt('200+50=');
+
+
+let misolFirst_anw = document.querySelectorAll('.misolFirst_anser')[0],
+    misolSecond_answ = document.querySelectorAll('.misolSecond__answers')[0]
+    misolThird_answ = document.querySelectorAll('.misolThird_answers')[0],
+    misolfourth_answ = document.querySelectorAll('.misolFourth__answer')[0],
+    misolFifth_answ = document.querySelectorAll('.misolFifsth-answer')[0];
+
+
+misolFirst_anw.addEventListener(function defender(){
+    
+})
